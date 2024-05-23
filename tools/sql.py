9d452952c53f4bc049444bb12ad43987900b16c7
Vinh Le <lethanhvinh95@gmail.com>
2024-05-23 07:59:30 +0300

    refactor: use @tool wrapper to make the tool declaration cleaner

--- tools/sql.py
+++ tools/sql.py
@@ -1,16 +1,8 @@
 import sqlite3
-from langchain.tools import Tool
+from langchain.tools import tool
 from pydantic.v1 import BaseModel
 
 conn = sqlite3.connect("db.sqlite")
-
-def run_sqlite_query(query):
-  try:
-    c = conn.cursor()
-    c.execute(query)
-    return c.fetchall()
-  except Exception as e:
-    return f"An exception occurred {str(e)}"
 
 def list_tables():
   c = conn.cursor()
@@ -18,33 +10,39 @@ def list_tables():
   tables = c.fetchall()
   return tuple(item[0] for item in tables)
 
-def describe_tables(tables: list[str]):
+
+class RunQueryToolArgSchema(BaseModel):
+  query: str
+
+
+@tool(
+  args_schema=RunQueryToolArgSchema
+)
+def run_query_tool(query):
+  """
+  Given a query string, run the query and return the result.
+  """
+  try:
+    c = conn.cursor()
+    c.execute(query)
+    return c.fetchall()
+  except Exception as e:
+    return f"An exception occurred {str(e)}"
+  
+  
+class DescribeTableSchema(BaseModel):
+  tables: list[str]
+
+@tool(
+  args_schema=DescribeTableSchema,
+)
+def describe_tables_tool(tables: list[str]):
+  """
+  Given a list of table names, return SQL schema of these tables
+  """
   sql_table_list = ', '.join([f"'{table}'" for table in tables])
   query = f"SELECT sql FROM sqlite_master WHERE type='table' AND name IN ({sql_table_list})"
 
   c = conn.cursor()
   rows = c.execute(query)
   return '\n'.join(row[0] for row in rows if row[0] is not None)
-
-
-class RunQueryToolArgSchema(BaseModel):
-  query: str
-
-
-run_query_tool = Tool.from_function(
-  name="run_sqlite_query",
-  description="Given a query string, run the query and return the result.",
-  func=run_sqlite_query,
-  args_schema=RunQueryToolArgSchema
-)
-
-
-class DescribeTableSchema(BaseModel):
-  tables: list[str]
-
-describe_tables_tool = Tool.from_function(
-  name="describe_table_tool",
-  description="Given a list of table names, return SQL schema of these tables",
-  func=describe_tables,
-  args_schema=DescribeTableSchema,
-)
